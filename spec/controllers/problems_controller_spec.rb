423960ab323a5f420d08f065b8cae4c93292cee5
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-09 11:15:45 +1200

    Assert problems are listed on the page

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -3,10 +3,24 @@ require 'rails_helper'
 RSpec.describe ProblemsController, type: :controller do
   describe "#index" do
     subject { assigns(:problems) }
-    before { get :index }
 
-    it "lists problems to solve" do
-      expect(subject).to eq []
+    context "when there are no problems to solve" do
+      before { get :index }
+
+      it "shows empty list if there are no problems available" do
+        expect(subject).to eq []
+      end
+    end
+
+    context "when problems are created" do
+      before do
+        Problem.create!
+        get :index
+      end
+
+      it "shows up on the list" do
+        expect(subject.size).to be 1
+      end
     end
   end
 end
