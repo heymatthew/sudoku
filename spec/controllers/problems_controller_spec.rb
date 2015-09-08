6101fce2b79a4692eb56c0de1267c3d52bc38cac
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-09 11:06:49 +1200

    Initial tests for problems index

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProblemsController, type: :controller do
-  describe "#show" do
+  describe "#index" do
+    subject { assigns(:problems) }
+    before { get :index }
+
+    it "lists problems to solve" do
+      expect(subject).to eq []
+    end
   end
 end
