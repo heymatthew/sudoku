4e9d4f0e5cb37ffdf81053fc4e037be49adaf8a5
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-09 11:25:49 +1200

    Templates // List problems, link to problems

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,19 @@
 class ProblemsController < ApplicationController
+  before_filter :fudge_problem_list
+
   def index
     @problems = Problem.all
+  end
+
+  def show
+  end
+
+  private
+
+  # TODO load these in from someplace else
+  def fudge_problem_list
+    if Problem.all.count < 10
+      Problem.create!
+    end
   end
 end
