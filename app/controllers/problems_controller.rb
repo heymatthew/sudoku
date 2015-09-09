bea756bf0ad1c29cdb3c6c9bce42b66413c753f3
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-09 14:58:37 +1200

    Look up problems when arriving on a page

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,19 +1,15 @@
 class ProblemsController < ApplicationController
-  before_filter :fudge_problem_list
-
   def index
     @problems = Problem.all
   end
 
   def show
+    @problem = Problem.find(problem_id)
   end
 
   private
 
-  # TODO load these in from someplace else
-  def fudge_problem_list
-    if Problem.all.count < 10
-      Problem.create!
-    end
+  def problem_id
+    params.require(:id)
   end
 end
