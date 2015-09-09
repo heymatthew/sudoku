08e1f645a1291ce049523b4d87cac7051fc633be
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-10 08:43:11 +1200

    One liner to create grid

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,17 +1,5 @@
 class Problem < ActiveRecord::Base
-  DEFAULT_GRID = [
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-    [nil,nil,nil, nil,nil,nil, nil,nil,nil],
-  ]
+  DEFAULT_GRID = [[nil]*9]*9
 
   serialize :grid
 end
