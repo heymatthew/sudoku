937f3d254634295207cf6862d2410b7924019593
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-11 09:27:53 +1200

    Generalised shared_examples into a file to stop duplicates

--- spec/concepts/row_spec.rb
+++ spec/concepts/row_spec.rb
@@ -1,26 +1,5 @@
 require 'rails_helper'
-
-RSpec.shared_examples "is #valid?" do
-  it "is #valid?" do
-    expect(subject).to be_valid
-  end
-
-  it "has no errors" do
-    expect { subject.valid? }.to_not change { subject.errors }.from([])
-  end
-end
-
-RSpec.shared_examples "is not #valid?" do |expected_error_regex|
-  it "is not #valid?" do
-    expect(subject).to_not be_valid
-  end
-
-  it "has a sensible error" do
-    expect { subject.valid? }
-      .to change { subject.errors }
-      .to include expected_error_regex
-  end
-end
+require_relative 'shared_examples'
 
 RSpec.describe Row do
   let(:row_number) { 4 }
