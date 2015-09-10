cf837dd643903a2d498506719116ed4caea4eac9
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-10 16:01:18 +1200

    Assert correct length

--- app/concepts/number_group.rb
+++ app/concepts/number_group.rb
@@ -1,9 +1,31 @@
 class NumberGroup
-  def initialize(list)
-    @list = list
+  attr_reader :errors
+  VALID_NUMBERS = (1..9)
+
+  def initialize(items)
+    @items = items
+    @errors = []
   end
 
   def valid?
-    @list.uniq.length == @list.length
+    if ( !correct_length? )
+      errors.push "less than #{expected_length} items in group"
+    end
+
+    errors.none?
+  end
+
+  private
+
+  def correct_length?
+    @items.length == expected_length
+  end
+
+  def items_unique?
+    @items.uniq.length == @items.length
+  end
+
+  def expected_length
+    @expected_length ||= VALID_NUMBERS.to_a.size
   end
 end
--- spec/concepts/number_group_spec.rb
+++ spec/concepts/number_group_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
+RSpec.shared_examples "is not #valid?" do
+  it "is not #valid?" do
+    expect(subject).to_not be_valid
+  end
+end
+
 RSpec.describe NumberGroup do
   let(:list) { [1,2,3,4,5,6,7,8,9] }
   subject { NumberGroup.new(list) }
 
   context "list has unique values 1-9" do
     it "is #valid?" do
-      puts "omg"
       expect(subject).to be_valid
     end
   end
+
+  context "has less than 9 numbers" do
+    let(:list) { [1,2,3] }
+    include_examples "is not #valid?"
+  end
+
 end
