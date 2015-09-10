820c66f332189a91c474e8e04a1172a5ca5fcc58
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-11 11:34:05 +1200

    Setup columns

---
+++ app/concepts/column.rb
@@ -0,0 +1,21 @@
+class Column < NumberGroup
+  def initialize(grid, column_number)
+    @grid, @column_number = grid, column_number
+    super(items)
+  end
+
+  def items
+    9.times.map do |row_index|
+      item_at(row_index, column_index)
+    end
+  end
+
+  def column_index
+    @column_number - 1
+  end
+
+  def item_at(row_index, column_index)
+    index = (row_index * Problem::GRID_WIDTH) + column_index
+    @grid[index]
+  end
+end
