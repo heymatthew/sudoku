90dfc7146bc541673a80361b05c5cef0b52ee96a
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-10 13:42:04 +1200

    Submit form params

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,15 +1,28 @@
 class ProblemsController < ApplicationController
+  before_filter :lookup_problem, :except => :index
+
   def index
     @problems = Problem.all
   end
 
   def show
-    @problem = Problem.find(problem_id)
+  end
+
+  def update
+    render :show
   end
 
   private
 
+  def lookup_problem
+    @problem = Problem.find(problem_id)
+  end
+
   def problem_id
     params.require(:id)
+  end
+
+  def cell_params
+    params.require(:cell)
   end
 end
