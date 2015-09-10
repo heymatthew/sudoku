c060ed6fc67bdb628dc60d3b6a362aa8eed1a157
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-11 09:11:02 +1200

    Initial work on row concept

---
+++ app/concepts/row.rb
@@ -0,0 +1,22 @@
+class Row < NumberGroup
+  def initialize(grid, row_number)
+    @grid, @row_number = grid, row_number
+    super(items)
+  end
+
+  def items
+    @grid.slice(row_range)
+  end
+
+  def row_range
+    (start_of_row...end_of_row)
+  end
+
+  def start_of_row
+    Problem::GRID_WIDTH * (@row_number - 1)
+  end
+
+  def end_of_row
+    start_of_row + Problem::GRID_WIDTH
+  end
+end
