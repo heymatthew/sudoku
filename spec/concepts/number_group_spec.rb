6af276b613d7abb4cd13f3bff1f96c3ad8dc315f
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-10 16:15:53 +1200

    Assert no duplicates

--- spec/concepts/number_group_spec.rb
+++ spec/concepts/number_group_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
-RSpec.shared_examples "is not #valid?" do
+RSpec.shared_examples "is not #valid?" do |expected_error_regex|
   it "is not #valid?" do
     expect(subject).to_not be_valid
+  end
+
+  it "has a sensible error" do
+    expect { subject.valid? }
+      .to change { subject.errors }
+      .to include expected_error_regex
   end
 end
 
 RSpec.describe NumberGroup do
   let(:list) { [1,2,3,4,5,6,7,8,9] }
-  subject { NumberGroup.new(list) }
+  let(:service) { NumberGroup.new(list) }
+  subject { service }
 
   context "list has unique values 1-9" do
     it "is #valid?" do
@@ -18,7 +25,11 @@ RSpec.describe NumberGroup do
 
   context "has less than 9 numbers" do
     let(:list) { [1,2,3] }
-    include_examples "is not #valid?"
+    include_examples "is not #valid?", /9 items/
   end
 
+  context "contains duplicates" do
+    let(:list) { [1,2,3,4,5,6,7,8,8] }
+    include_examples "is not #valid?", /duplicate/
+  end
 end
