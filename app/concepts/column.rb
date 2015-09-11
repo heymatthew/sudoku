6a2d3bcebb6d82f0cf29fcf8e4d75b932ac5e84b
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-11 16:40:46 +1200

    Colum now uses grid

--- app/concepts/column.rb
+++ app/concepts/column.rb
@@ -5,17 +5,12 @@ class Column < NumberGroup
   end
 
   def items
-    9.times.map do |row_index|
-      item_at(row_index, column_index)
+    Grid::HEIGHT.times.map do |row_index|
+      @grid[row_index][column_index]
     end
   end
 
   def column_index
     @column_number - 1
-  end
-
-  def item_at(row_index, column_index)
-    index = (row_index * Grid::WIDTH) + column_index
-    @grid[index]
   end
 end
