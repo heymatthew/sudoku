2f72769449ddc6ae29a02e37147933d742f4cc3d
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-11 15:04:39 +1200

    Initial work on Grid concept

--- app/concepts/grid.rb
+++ app/concepts/grid.rb
@@ -1,4 +1,27 @@
 class Grid
+  attr_accessor :errors
+
   WIDTH = 9
   HEIGHT = 9
+
+  def initialize(cells)
+    @cells = cells
+    @errors = []
+  end
+
+  def valid?
+    if not_enough_cells?
+      errors.push "need #{expected_grid_size} to play sudoku"
+    end
+
+    errors.none?
+  end
+
+  def not_enough_cells?
+    @cells.count != expected_grid_size
+  end
+
+  def expected_grid_size
+    WIDTH * HEIGHT
+  end
 end
