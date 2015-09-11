db2edfe12488a1ba2a68313c3375f85b50ca1cf0
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-11 16:38:29 +1200

    Row concept takes a grid now

--- app/concepts/row.rb
+++ app/concepts/row.rb
@@ -5,18 +5,10 @@ class Row < NumberGroup
   end
 
   def items
-    @grid.slice(row_range)
+    @grid[row_index]
   end
 
-  def row_range
-    (start_of_row...end_of_row)
-  end
-
-  def start_of_row
-    Grid::WIDTH * (@row_number - 1)
-  end
-
-  def end_of_row
-    start_of_row + Grid::WIDTH
+  def row_index
+    @row_number - 1
   end
 end
