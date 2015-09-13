732a1ee2072f1be5658afa46f028c06f7e84fff4
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-14 09:18:30 +1200

    Answer checks columns, rows and boxes

--- app/concepts/answer.rb
+++ app/concepts/answer.rb
@@ -6,6 +6,8 @@ class Answer
   WIDTH = 9
   HEIGHT = 9
 
+  GRID_SQUARES = (1..3).to_a.product (1..3).to_a
+
   def initialize(cells)
     @cells = cells
     @errors = []
@@ -18,9 +20,43 @@ class Answer
   def valid?
     if not_enough_cells?
       errors.push "need #{expected_grid_size} to play sudoku"
+    else
+      check_grid_rows
+      check_grid_columns
+      check_grid_squares
     end
 
     errors.none?
+  end
+
+  def check_grid_rows
+    (1..HEIGHT).each do |row_number|
+      row = Row.new(self, row_number)
+
+      if !row.valid?
+        errors.push(*row.errors)
+      end
+    end
+  end
+
+  def check_grid_columns
+    (1..WIDTH).each do |column_number|
+      column = Column.new(self, column_number)
+
+      if !column.valid?
+        errors.push(*column.errors)
+      end
+    end
+  end
+
+  def check_grid_squares
+    GRID_SQUARES.each do |row_number, column_number|
+      box = Box.new(self, row_number, column_number)
+
+      if !box.valid?
+        errors.push(*box.errors)
+      end
+    end
   end
 
   def not_enough_cells?
