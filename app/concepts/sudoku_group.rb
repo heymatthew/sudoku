f9871a57f90510d468b9e7049d14c6b317c63fac
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-14 15:06:50 +1200

    alsdjfasdfkadskadlajsfsj

--- app/concepts/sudoku_group.rb
+++ app/concepts/sudoku_group.rb
@@ -5,22 +5,34 @@ SudokuGroup = Struct.new(:items) do
 
   def valid?
     errors = []
-    errors.push(*empty_errors)
-    errors.push(*duplicate_errors)
-    errors.push(*invalid_errors)
-
+    check_for_duplicate_values
+    check_for_invalid_values
     errors.none?
   end
 
-  def empty_items?
-    items.select(&:empty?).size > 0
+  def filled_out_cells
+    @filled_out_cells ||= items.reject(&:empty?)
   end
 
-  def duplicate_items?
-    items.count != items.uniq.count
+  def check_for_duplicate_values
+    if duplicate_values.any?
+      "duplicate values: #{duplicate_values.to_sentence}"
+    end
   end
 
-  def invalid_items?
-    items.select { |item| VALID_RANGE.include?(item.to_i) }
+  def check_for_invalid_values
+    if invalid_items.any?
+      "invalid values: #{invalid_items.to_sentence}"
+    end
+  end
+
+  def duplicate_values
+    filled_out_cells.group_by { |item| item }
+      .select { |item, matching| matching.count > 1 }
+      .keys
+  end
+
+  def invalid_items
+    filled_out_cells.select { |item| VALID_RANGE.include?(item.to_i) }
   end
 end
