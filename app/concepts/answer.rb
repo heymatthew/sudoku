dd55d647dd9f5b624d8b0eef9e351496e02f67de
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-14 14:33:45 +1200

    Tripple code review, redoing all the things

--- app/concepts/answer.rb
+++ app/concepts/answer.rb
@@ -1,73 +1,66 @@
 class Answer
   attr_accessor :errors
 
-  delegate :slice, to: :grid
-
-  WIDTH = 9
+  WIDTH  = 9
   HEIGHT = 9
 
-  GRID_SQUARES = (1..3).to_a.product (1..3).to_a
+  SUBGRID_DIMENSIONS = [(0..2),(3..5),(6..8)].product [(0..2),(3..5),(6..8)]
 
   def initialize(cells)
     @cells = cells
     @errors = []
   end
 
-  def [](index)
-    grid[index]
+  def grid
+    @grid ||= @cells.each_slice(WIDTH).to_a
+  end
+
+  def columns
+    grid.transpose.map { |column| SudokuGroup.new(column) }
+  end
+
+  def rows
+    grid.map { |row| SudokuGroup.new(row) }
+  end
+
+  def subgrids
+    SUBGRID_DIMENSIONS.map do |grid_row_range, grid_column_range|
+      subgrid = grid.slice(grid_row_range).transpose.slice(grid_column_range)
+      SudokuGroup.new(subgrid)
+    end
   end
 
   def valid?
+    errors = []
+
     if not_enough_cells?
-      errors.push "need #{expected_grid_size} to play sudoku"
+      errors.push "need #{expected_cell_count} to play sudoku"
     else
-      check_grid_rows
-      check_grid_columns
-      check_grid_squares
+      errors.push(*row_errors)
+      errors.push(*column_errors)
+      errors.push(*subgrid_errors)
     end
 
     errors.none?
   end
 
-  def check_grid_rows
-    (1..HEIGHT).each do |row_number|
-      row = Row.new(self, row_number)
-
-      if !row.valid?
-        errors.push(*row.errors)
-      end
-    end
-  end
-
-  def check_grid_columns
-    (1..WIDTH).each do |column_number|
-      column = Column.new(self, column_number)
-
-      if !column.valid?
-        errors.push(*column.errors)
-      end
-    end
-  end
-
-  def check_grid_squares
-    GRID_SQUARES.each do |row_number, column_number|
-      box = Box.new(self, row_number, column_number)
-
-      if !box.valid?
-        errors.push(*box.errors)
-      end
-    end
-  end
-
   def not_enough_cells?
-    @cells.count != expected_grid_size
+    @cells.count != expected_cell_count
   end
 
-  def expected_grid_size
+  def expected_cell_count
     WIDTH * HEIGHT
   end
 
-  def grid
-    @grid ||= @cells.each_slice(WIDTH).to_a
+  def row_errors
+    rows.reject(&:valid).map(:errors)
+  end
+
+  def column_errors
+    columns.reject(&:valid).map(:errors)
+  end
+
+  def subgrid_errors
+    subgrids.reject(&:valid).map(:errors)
   end
 end
