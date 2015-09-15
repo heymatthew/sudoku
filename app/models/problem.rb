682f2c5482e53e3d1b6a22f42a9a5072c6f2cdd0
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-16 10:41:43 +1200

    Use a real grid when serializing problems

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,21 @@
 class Problem < ActiveRecord::Base
-  DEFAULT_GRID = [[nil]*Grid::WIDTH]*Grid::HEIGHT
+  validates :cells, presence: true, format: {
+    with: /\A(?:\d?,){80}\d?\z/,
+    message: "requires 81 single didgits separated by commas"
+  }
 
-  # TODO define serialise functions
-  serialize :grid
+  def grid=(grid)
+    self.cells = grid.cells.join(",")
+  end
+
+  def grid
+    Grid.new(split_cells)
+  end
+
+  private
+
+  def split_cells
+    keep_trailing_empties = -1
+    cells.split(',', keep_trailing_empties)
+  end
 end
