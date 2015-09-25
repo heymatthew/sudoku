eacabf3c477a3d11956b40d4b51c7cf4714e625f
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-25 18:52:15 +1200

    Remove composition relationship between Problem and Grid

--- app/concepts/grid.rb
+++ app/concepts/grid.rb
@@ -1,38 +1,14 @@
 class Grid
+  include ActiveModel::Model
+
   SUBGRID_DIMENSIONS = [(0..2),(3..5),(6..8)].product([(0..2),(3..5),(6..8)])
   WIDTH = 9
   HEIGHT = 9
   SIZE = WIDTH * HEIGHT
 
-  delegate :size, to: :values
+  attr_accessor :cells
 
-  def initialize(values)
-    if values.count != SIZE
-      raise ArgumentError, "initial values don't match expected size"
-    end
-
-    @values = values.clone
-  end
-
-  def compose_with_guess(guess)
-    if guess.count != SIZE
-      raise ArgumentError, "composed guess doesn't match expected size"
-    end
-
-    cells.zip(guess).each do |cell, guess_value|
-      cell.value = guess_value
-    end
-
-    self
-  end
-
-  def values
-    @values.clone
-  end
-
-  def cells
-    @cells ||= construct_cells_from_values
-  end
+  validates :cells, length: { is: SIZE }
 
   def rows
     @rows ||= cells.each_slice(WIDTH).to_a
@@ -49,12 +25,6 @@ class Grid
   end
 
   def complete?
-    @cells.all?(&:set?)
-  end
-
-  private
-
-  def construct_cells_from_values
-    @values.map { |value| Cell.new(value) }
+    values.select(&:nil?).none?
   end
 end
