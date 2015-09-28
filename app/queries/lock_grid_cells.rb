b8596fe541ed74eb0aedde428adb163b45b4af3c
Matthew Gray <matthew.gray@powershop.co.nz>
2015-09-29 11:33:24 +1300

    Lock grid cells service built

---
+++ app/queries/lock_grid_cells.rb
@@ -0,0 +1,20 @@
+class LockGridCells
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def call
+    Grid.new(cells: locked_cells)
+  end
+
+  private
+
+  def locked_cells
+    @grid.cells.map { |cell| lock_if_present(cell) }
+  end
+
+  def lock_if_present(cell)
+    cell.locked = cell.value.present?
+    cell
+  end
+end
